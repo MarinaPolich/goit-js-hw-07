17dffc309cb9b808009602a7e2ead1ac25721c5b
MarinaPolich <marina.polich@gmail.com>
2022-09-18 23:57:41 +0300

    add gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const cardsGallery = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", cardsGallery);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class = "gallery__image"
+                    src = "${preview}"
+                    data-source = "${original}"
+                    alt = "${description}"
+                    />
+                </a>
+            </div>`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+  
+  let urlBigImg = event.target.dataset.source;
+  console.log(urlBigImg);
+
+  const instance = basicLightbox.create(`
+    <img src="${urlBigImg}" width="800" height="600">
+    `);
+
+  document.addEventListener("keydown", closeModalWindow);
+  instance.show();
+
+  function closeModalWindow(event) {
+    if (event.code === "Escape") {
+      document.removeEventListener("keydown", closeModalWindow);
+      instance.close();
+      console.log(event);
+    }
+  }
+}
